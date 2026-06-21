9e6c7570d7b213df5c9a69b264f908337f1d434c
Helen Allien Poe <besteto@gmail.com>
2026-06-21 22:45:56 +0400

    [Config][*] upgrade engine to 5.8, ignore .slnx solution files

--- Source/ThroughArcaneEyesEditor.Target.cs
+++ Source/ThroughArcaneEyesEditor.Target.cs
@@ -8,8 +8,8 @@ public class ThroughArcaneEyesEditorTarget : TargetRules
 	public ThroughArcaneEyesEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("ThroughArcaneEyes");
 	}
 }
